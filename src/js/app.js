14dd75e308f6b38156447169759c024a202246c6
Cusatelli <einarvandevelde@gmail.com>
2022-01-14 13:14:26 +0100

    Fix missunderstanding of text. Can also send money to bank when active loan.

--- src/js/app.js
+++ src/js/app.js
@@ -63,14 +63,14 @@ const hideBankLoanSection = () => {
     payLoanButtonElement.style.display = "none";
     bankLoanSectionElement.style.display = "none";
     repayLoanButtonElement.style.display = "none";
-    bankButtonElement.style.display = "inline";
+    //bankButtonElement.style.display = "inline";
 }
 
 const showBankLoanSection = () => {
     payLoanButtonElement.style.display = "inline";
     bankLoanSectionElement.style.display = "flex";
     repayLoanButtonElement.style.display = "inline";
-    bankButtonElement.style.display = "none";
+    //bankButtonElement.style.display = "none";
 }
 
 const handleGetLoanButtonEvent = e => {
